80849bc3638b9d11e74c0a1c4f16e78336d11ec0
Joe King <kingje@vcu.edu>
2019-05-26 15:53:10 -0400

    Created initial components and stubbed json data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ApplicationDetailComponent } from './application-detail/application-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "application-detail",
+    component: ApplicationDetailComponent,
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
